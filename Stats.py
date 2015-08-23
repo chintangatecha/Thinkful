6ba6f8511db3cee127835e02a061ebd613cd3749
chintangatecha <chintangatecha@yahoo.co.in>
2015-08-23 10:21:35 +1000

    stats file added/modified

--- Stats.py
+++ Stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import scipy as scipy
 
 data = '''Region, Alcohol, Tobacco
 North, 6.47, 4.03
@@ -12,11 +13,26 @@ Southwest, 4.79, 2.71
 Wales, 5.27, 3.53
 Scotland, 6.08, 4.51
 Northern Ireland, 4.02, 4.56'''
-data.split('\n')
-#data = [i.split(', ') for i in data]
-column_names = data[0] # this is the first row
-data_rows = data[1::] # these are all the following rows of data
-#df = pd.DataFrame(data_rows, columns=column_names)
 
-print(data[0])
-print(data)
+data = data.splitlines()
+data = [i.split(', ') for i in data]
+column_names = data[0]
+data_rows = data[1::] # these are all the following rows of data
+df = pd.DataFrame(data_rows, columns=column_names)
+df['Alcohol'] = df['Alcohol'].astype('float')
+df['Tobacco'] = df['Tobacco'].astype('float')
+df['Alcohol'].mean()
+df['Alcohol'].median()
+df['Alcohol']
+
+def calculatemean(list1):
+	for i in list1:
+		print("Mean of {0} is {1}".format(i,df[i].mean()))
+		#print("Mode of {0} is {1}".format(i,stats.mode(df[i])))
+		print("median of {0} is {1}".format(i,df[i].median()))
+		print("standard deviation of {0} is {1}".format(i,df[i].std()))
+		print("Variance of {0} is {1}".format(i,df[i].var()))
+		print("Range of {0} is {1}".format(i,max(df[i]) - min(df[i])))
+list2 = ['Tobacco','Alcohol']
+#print(stats.mode(df['Tobacco']) )   name 'stats' is not defined.
+calculatemean(list2)
